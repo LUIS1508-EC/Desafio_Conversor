016a83fcc45bef381c3b61ca2c42f4942d44037f
LUIS1508-EC <151102084+LUIS1508-EC@users.noreply.github.com>
2024-04-07 16:37:28 -0600

    .....

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,39 +8,39 @@ const find = async (monedas) => {
         const respuesta = await fetch(`https://mindicador.cl/api/${monedas}`);
         const { serie: data } = await respuesta.json();
         return { lastValue: data[0].valor, allValue: data };
-      
-} catch (error) {
-    console.log(error)
-}
+
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 
 const convertirmonedas = async () => {
     try {
-        if(!origen_moneda.value) {alert("Ingrese el monto en Pesos"); return;}
-        if(!selecciona_moneda) {alert("Seleccione Moneda"); return;}
+        if (!origen_moneda.value) { alert("Ingrese el monto en Pesos"); return; }
+        if (!selecciona_moneda) { alert("Seleccione Moneda"); return; }
 
         const datos = await find(selecciona_moneda.value)
 
         resultado.innerHTML = `Resultado: $${(origen_moneda.value / datos.lastValue).toFixed(2)}`
-getDataLabelChart(datos.allValue);
+        getDataLabelChart(datos.allValue);
 
-    } catch (error){
+    } catch (error) {
         console.log(error)
     }
 }
 
 // Crear gráfico sin data
-let diezultimos = new Chart(cuadro_monedas, {
+let anteriores = new Chart(cuadro_monedas, {
     type: 'line',
-    data: { labels: [], datasets: [{ label: 'Historial últimos 10 días', data: [], borderWidth: 1 }] },
+    data: { labels: [], datasets: [{ label: 'Historial últimos 10 días', data: [], borderWidth: 3 }] },
     options: { scales: { y: { beginAtZero: false } } }
 });
 
 const getDataLabelChart = async (data) => {
     try {
         // Eliminar grafico anterior
-        diezultimos.destroy();
+        anteriores.destroy();
         // Rescatar datos para mostrar en grafico
         const tabla = await data.map(label => new Intl.DateTimeFormat('es-CL').format(new Date(label.fecha)));
         const datostabla = await data.map(data => data.valor);
@@ -48,7 +48,7 @@ const getDataLabelChart = async (data) => {
         const ultimosdiez = tabla.splice(0, 10).reverse();
         const lastTenData = datostabla.splice(0, 10).reverse();
         // Crear nuevo grafico con datos rescatados
-        diezultimos = new Chart(cuadro_monedas, {
+        anteriores = new Chart(cuadro_monedas, {
             type: 'line',
             data: { labels: ultimosdiez, datasets: [{ label: 'Historial últimos 10 días', data: lastTenData, borderWidth: 1 }] },
             options: { scales: { y: { beginAtZero: false } } }
